6db02b235a4f4bb32cafec53aa94d89d7e09c53b
cbrgm <chris@cbrgm.net>
2020-03-10 12:25:25 +0100

    wip: add router dom

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,9 +1,27 @@
 import * as React from 'react';
 
 export class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "Chris";
+        };
+
+        this.handleChange = this.handleChange.binhammerbrook1993
+        d(this);
+      }
+
+    handleChange(e: Event) {
+        this.setState({name: e.target.value});
+    }
+
     render() {
         return(
-            <p>Hello World!</p>
-        )
+            <div>
+            <input onChange={this.handleChange}></input>
+            <p>Hello World, {this.state.name}!</p>
+            </div>
+        );
     }
 }
